8f2b84d6c132080e579a9476bc0484564461051b
Arman Bilge <armanbilge@gmail.com>
2014-05-10 22:08:07 +1200

    Add code for marking zones

--- elevation.R
+++ elevation.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
-df = read.csv("elevation.csv")
-distance = tail(df$horizontal, n = 1) - df$horizontal
-elevation = tail(df$vertical, n = 1) - df$vertical
+df <- read.csv("elevation.csv")
+distance <- tail(df$horizontal, n = 1) - df$horizontal
+elevation <- tail(df$vertical, n = 1) - df$vertical
 
 pdf(file = "elevation.pdf", 7, 2.8)
 
@@ -12,3 +12,21 @@ plot(distance, elevation, type = "o", asp = 1, las = 1,
      yaxp = c(0,3,3),
      xlab = "Distance from the low tide mark (m)",
      ylab = "Elevation above low tide (m)")
+
+y <- function(x) {
+    x.max <- tail(distance[x <= distance], n = 1)
+    x.min <- distance[x > distance][1]
+    y.max <- tail(elevation[x <= distance], n = 1)
+    y.min <- elevation[x > distance][1]
+    print(c(x.max, x.min, y.max, y.min))
+    return((y.max - y.min) / (x.max - x.min) * (x - x.min) + y.min)
+}
+print(elevation)
+print(distance)
+zones <- read.csv("zones.csv")
+for(i in 1:nrow(zones)) {
+    name <- zones$name[i]
+    coord <- tail(df$horizontal, n = 1) - zones$coordinate[i] 
+    print(y(coord))
+    segments(coord, -0.2, y1 = y(coord))
+} 
