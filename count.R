e8b9cab0e937b9e14f4ed0656a24c35d7d754d47
Arman Bilge <armanbilge@gmail.com>
2014-05-16 00:55:15 +1200

    Add code to do rank correlation test

--- count.R
+++ count.R
@@ -8,12 +8,20 @@ zone <- list(read.csv("zone1.csv"),
              read.csv("zone4.csv"),
              read.csv("zone5.csv"))
 
+count <- c(nrow(zone[[1]]),
+           nrow(zone[[2]]),
+           nrow(zone[[3]]),
+           nrow(zone[[4]]),
+           nrow(zone[[5]]))
+
+r <- cor(count, 1:5, method = "spearman")
+print(r) 
+n <- 30 # TODO verify value of n
+T <- r * sqrt((n-2)/(1-r^2))
+print(T)
+
 pdf("count.pdf", 8, 5)
-barplot(c(nrow(zone[[1]]),
-          nrow(zone[[2]]),
-          nrow(zone[[3]]),
-          nrow(zone[[4]]),
-          nrow(zone[[5]])),
+barplot(count,
         names.arg = zone.names,
         ylim = c(0, 20),
         xlab = "Zone",
